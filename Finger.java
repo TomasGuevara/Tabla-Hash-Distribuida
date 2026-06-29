86367ef4c3a6d0c56fe8baca2f25bccdafa01cac
renzo_guevara <renzoguevara@cnea.gob.ar>
2026-06-29 13:51:15 -0300

    Se comentó todo el codigo donde faltaban comentarios

--- Finger.java
+++ Finger.java
@@ -1,5 +1,21 @@
 import java.math.BigInteger;
 
+/* Represents an entry in the Chord finger table.
+ *
+ * Each finger entry contains:
+ *
+ *  - start: the beginning of the interval associated
+ *           with this finger, computed as
+ *           (n + 2^i) mod 2^M.
+ *
+ *  - nodeId: the identifier of the successor node
+ *            responsible for the start value.
+ *
+ *  - port: the communication port of the successor node.
+ *
+ * Finger tables are used by Chord to perform efficient
+ * key lookups in O(log N) hops.
+ */
 class Finger {
     BigInteger start;
     BigInteger nodeId;
